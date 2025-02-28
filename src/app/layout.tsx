818567270a846669c7e8bdfab69819bc6521fa42
E <e@elzr.me>
2025-02-27 22:40:42 -0500

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,24 @@ import { fontsClassName } from "@/styles/fonts"
 
 export const metadata: Metadata = {
   title: "Backboard",
+  description: "Primitive productivity",
+  icons: {
+    icon: [
+      { rel: "apple-touch-icon", sizes: "57x57", url: "/icons/apple-icon-57x57.png" },
+      { rel: "apple-touch-icon", sizes: "60x60", url: "/icons/apple-icon-60x60.png" },
+      { rel: "apple-touch-icon", sizes: "72x72", url: "/icons/apple-icon-72x72.png" },
+      { rel: "apple-touch-icon", sizes: "76x76", url: "/icons/apple-icon-76x76.png" },
+      { rel: "apple-touch-icon", sizes: "114x114", url: "/icons/apple-icon-114x114.png" },
+      { rel: "apple-touch-icon", sizes: "120x120", url: "/icons/apple-icon-120x120.png" },
+      { rel: "apple-touch-icon", sizes: "144x144", url: "/icons/apple-icon-144x144.png" },
+      { rel: "apple-touch-icon", sizes: "152x152", url: "/icons/apple-icon-152x152.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", url: "/icons/apple-icon-180x180.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", url: "/icons/android-icon-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/icons/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "96x96", url: "/icons/favicon-96x96.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", url: "/icons/favicon-16x16.png" },
+    ],
+  },
 }
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
