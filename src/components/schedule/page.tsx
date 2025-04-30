ae7d6d52c35dde203f02244ae14cb5baada3febe
E <e@elzr.me>
2025-04-30 15:20:53 -0400

    add back suspsense to page

--- src/components/schedule/page.tsx
+++ src/components/schedule/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
+import { Suspense } from "react"
 import TimeslotModal from "./timeslot-modal"
 import Week from "./week"
 
 export default function SchedulePage() {
   return (
-    <>
+    <Suspense>
       <Week />
       <TimeslotModal />
-    </>
+    </Suspense>
   )
 }
